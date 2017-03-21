89cfc74c52472a33e3c195793975f189cd20372a
NoranM <NoranM@DEV-NORANM>
2017-03-21 16:51:31 +0200

    Add project files.

---
+++ MVCDemo_EF/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo_EF.Models
+{
+   [MetadataType(typeof(EmployeeMetaData))]
+    public partial class Employee
+    {
+    }
+
+    public class EmployeeMetaData {
+        [Required]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public int DepartmentId { get; set; }
+
+    }
+}
